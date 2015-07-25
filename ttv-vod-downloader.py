1b56faa58bedda30e3f21c1d6f5c91c10fc0aa14
Kirill Golenickij <sterk12@gmail.com>
2015-07-26 00:24:14 +0300

    improve main script
    Added parameter to change saving path.
    Improve parameter handing and change help messages.

--- ttv-vod-downloader.py
+++ ttv-vod-downloader.py
@@ -7,8 +7,9 @@ import sys
 from twitch_api import download_ids
 from twitch_api import download_all
 import time
+import os
 
-parser = argparse.ArgumentParser(description='Download the vod by id, all vods by channel name.')
+parser = argparse.ArgumentParser(description='Download specific Twitch broadcasts by id or all from specific channel.')
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-i', '--ids',
                    nargs='+',
@@ -31,13 +32,20 @@ parser.add_argument('-t', '--threads',
                     type=int,
                     default=20,
                     help='number of downloading threads (default: %(default)s)')
-
-if len(sys.argv) == 1:
-    parser.print_help()
-    sys.exit(1)
+parser.add_argument('-p', '--path',
+                    metavar='PATH',
+                    default='.',
+                    help='path to directory where to save broadcasts')
 
 args = vars(parser.parse_args())
 start = time.clock()
+
+if args['id'] is None and args['ids'] is None:
+    print('At least one id required.\nUsage: %s --help' % sys.argv[0])
+    sys.exit(1)
+
+if args['path']:
+    os.chdir(args['path'])
 if args['id']:
     print('Download v%i...\n' % args['id'])
     download_ids([args['id']], resume=args['continue'], num_threads=args['threads'])
@@ -47,4 +55,5 @@ elif args['ids']:
 elif args['channel_name']:
     print('Download vods from channel %s...\n' % args['channel_name'])
     download_all(args['channel_name'], resume=args['continue'], num_threads=args['threads'])
-print('\nTime elapsed: ' + str(time.clock() - start) + ' sec.')
+
+print('\nTotal time taken: %.0f sec.' % str(time.clock() - start))
